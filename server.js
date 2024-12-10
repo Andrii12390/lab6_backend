a306b7d3eb6b97410103317d4fc95c94e7e5d01a
Andrii <pasiura.andriy@lll.kpi.ua>
2024-12-10 02:47:30 +0200

    fixed bug?

--- server.js
+++ server.js
@@ -19,7 +19,6 @@ app.use(bodyParser.json());
 
 const uri = "mongodb+srv://user_1:1234@cluster0.ckpfo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -29,14 +28,10 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
-    console.log('Starting server');
     await client.connect();
-
-    console.log('Connected to MongoDB');
 
     const db = client.db('mydatabase');
     const collection = db.collection('data');
-
     app.get('/data', async (req, res) => {
         try {
             const data = await collection.find({}).toArray();
@@ -71,30 +66,9 @@ async function run() {
     });
 
     const server = app.listen(process.env.PORT || 3000, () => {
-        const address = server.address();
-        const host = process.env.VERCEL_URL || 'localhost'; // Використовуємо VERCEL_URL, якщо доступний
-        const wsUrl = `wss://${host}:${address.port}`;
-        console.log(`HTTP Server is running on ${process.env.PORT ? 'Vercel' : 'http://localhost'}:${address.port}`);
-        console.log(`WebSocket Server is running at ${wsUrl}`);
+        console.log('Server is running')
     });
     
-    const wss = new WebSocketServer({ server });
-    
-    wss.on('connection', ws => {
-        console.log('New WebSocket connection');
-
-        ws.on('message', message => {
-            console.log('Received message:', message);
-        });
-
-        ws.on('close', () => {
-            console.log('WebSocket connection closed');
-        });
-
-        ws.on('error', error => {
-            console.error('WebSocket error:', error);
-        });
-    });
 
     function broadcastToClients(data) {
         wss.clients.forEach(client => {
